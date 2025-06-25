de92d5705db12434a29732564b639c44534ce763
JulesTopart <jules.topart@gmail.com>
2025-06-25 12:29:31 +0200

    wip(Global) : Add Imrad and TinyXML dependencies for project and scene saving.

---
+++ premake5.lua
@@ -0,0 +1,25 @@
+project "tinyxml2"
+    kind "StaticLib"
+    language "C"
+    staticruntime "on"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "*.c",
+        "*.cpp",
+        "*.h"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
